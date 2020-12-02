2b99eb9f469b09ab5ac14892e783e8711be4bf5f
kuesa <me@noahgrove.me>
2020-12-02 15:36:59 -0800

    Added drag + drop, visual gate class, and sample gates on canvas

--- src/org/ecs160/a2/Canvas.java
+++ src/org/ecs160/a2/Canvas.java
@@ -4,6 +4,7 @@ import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.geom.Dimension;
 
+
 /** Canvas
  *  The class that generates the area to place gates and wires, arranged in a Grid pattern.
  *  By default the canvas is 10000x10000px large and supports panning around.
@@ -12,13 +13,23 @@ public class Canvas extends Container {
     public Canvas() {
         super();
         getStyle().setBgTransparency(255);
-        getStyle().setBgColor(0x99CCCC);
+        getStyle().setBgColor(0x99cccc);
         setScrollableX(true);
         setScrollableY(true);
+        setDropTarget(true);
 
         // Set the viewport to the middle of the grid
         setScrollX(5000);
         setScrollY(5000);
+
+        addComponent(0, new Gate(GateType.POWER));
+        addComponent(1, new Gate(GateType.AND));
+        addComponent(2, new Gate(GateType.OR));
+        addComponent(3, new Gate(GateType.XOR));
+        addComponent(4, new Gate(GateType.NOT));
+        addComponent(5, new Gate(GateType.NAND));
+        addComponent(6, new Gate(GateType.NOR));
+        addComponent(7, new Gate(GateType.XNOR));
 
         createCells();
     }
@@ -33,6 +44,20 @@ public class Canvas extends Container {
     @Override
     public Dimension calcPreferredSize() {
         return new Dimension(10000,10000);
+    }
+
+    @Override
+    public void drop(Component dragged, int x, int y) {
+        int i = getComponentIndex(dragged);
+        Component dest = getComponentAt(x, y);
+        int destIdx = getComponentIndex(dest);
+        if(destIdx > -1 && dest != dragged && dest instanceof CanvasCell) {
+            removeComponent(dragged);
+            removeComponent(dest);
+            addComponent(destIdx, dragged);
+            addComponent(i, new CanvasCell());
+        }
+        animateLayout(1);
     }
 
     /** CanvasCell
